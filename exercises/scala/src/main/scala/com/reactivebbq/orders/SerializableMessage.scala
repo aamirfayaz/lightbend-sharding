a55dbf597cff1694bfe3d0a40537a5e3435ab0de
aamirfayaz <dropmeataamir@gmail.com>
2021-03-19 23:56:03 +0530

    stateful systems

--- exercises/scala/src/main/scala/com/reactivebbq/orders/SerializableMessage.scala
+++ exercises/scala/src/main/scala/com/reactivebbq/orders/SerializableMessage.scala
@@ -4,3 +4,9 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 
 @JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, property = "type")
 trait SerializableMessage
+
+/**
+  A Marker trait used by the serializer in order to determine what type of serializer to use for certain types of
+  messages types essentially.
+
+ */
